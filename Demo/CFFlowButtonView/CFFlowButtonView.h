2312cfa830b3b68cc5c0493970ec7c4f700ac8a8
summertian4 <zhoulingyu@zhoulingyudeMacBook-Pro.local>
2015-10-28 14:57:52 +0800

    更正一些注释
    更正一些注释

--- Demo/CFFlowButtonView/CFFlowButtonView.h
+++ Demo/CFFlowButtonView/CFFlowButtonView.h
@@ -15,6 +15,13 @@
  */
 @property (nonatomic, strong) NSMutableArray *buttonList;
 
+/**
+ *  通过传入一组按钮初始化CFFlowButtonView
+ *
+ *  @param buttonList 按钮数组
+ *
+ *  @return CFFlowButtonView对象
+ */
 - (instancetype)initWithButtonList:(NSMutableArray *)buttonList;
 
 @end
